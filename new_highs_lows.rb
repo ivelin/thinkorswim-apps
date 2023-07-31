01d4a1697dfcc3e69266ddbc479a55ede01447f0
Ivelin Ivanov <ivelin117@gmail.com>
2023-07-30 23:01:37 -0500

    Update new_highs_lows.rb
    Signed-off-by: Ivelin Ivanov <ivelin117@gmail.com>

--- new_highs_lows.rb
+++ new_highs_lows.rb
@@ -1,59 +1,61 @@
-# This indicator uses the Index New highs and Index New lows to help gauge overall Market sentiment. It's a leading indicator.
-# Index New High - New Low Indicator
-# Original script by Mobius
+# Based on original work by Melvin and Mobius
 # https://thinkscript101.com/new-highs-new-lows-indicator-thinkorswim/
-
+#
+# Copyright 2022 Scott J. Johnson (https://scottjjohnson.com)
+# Copyright 2023 Ivelin Ivanov (ivelin117@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+ 
+#
+# ExchangeListedNetHighLow
+#
+# Shows the number of new 52-week highs - new 52-week lows by day for the selected exchange. If the
+# number of lows is greater than the number of highs, the value will be negative. If highs are
+# greater, the number will be positive. Exchange = "ETF" shows net highs - lows for all ETFs listed
+# on the 4 exchanges.
+#
+# The TOS symbols don't appear to work at expected when the aggregation period is WEEK so this
+# study is only for daily charts.
+#
 declare lower;
-input Symb = {default "NYSE", "NASDQ", "AMEX", "ARCA", "ETF"};
-input length = 10;
-input OverSold = 20;
-input OverBought = 80;
-Input AvgType = AverageType.Hull;
-  
-def agg = AggregationPeriod.Day;
-def NYSEH  = close(Symbol = "$NYHGH", period = agg);
-def NYSEL  = close(Symbol = "$NYLOW", period = agg);
-def NASDQH = close(Symbol = "$NAHGH", period = agg);
-def NASDQL = close(Symbol = "$NALOW", period = agg);
-def AMEXH  = close(Symbol = "$AMHGH", period = agg);
-def AMEXL  = close(Symbol = "$AMLOW", period = agg);
-def ARCAH  = close(Symbol = "$ARHGH", period = agg);
-def ARCAL  = close(Symbol = "$ARLOW", period = agg);
-def ETFH   = close(Symbol = "$ETFHGH", period = agg);
-def ETFL   = close(Symbol = "$ETFLOW", period = agg);
-def P;
-Switch (Symb){
+input exchange = {default "NYSE", "NASDAQ", "AMEX", "ARCA", "ETF"};
+ 
+def ap = AggregationPeriod.DAY;
+def diff;
+switch (exchange){
 case "NYSE":
-P = NYSEH / (NYSEH + NYSEL) * 100;
-case "NASDQ":
-P = NASDQH / (NASDQH + NASDQL) * 100; 
+    diff = close(Symbol = "$NYHGH", period = ap) - close(Symbol = "$NYLOW", period = ap);
+case "NASDAQ":
+    diff = close(Symbol = "$NAHGH", period = ap) - close(Symbol = "$NALOW", period = ap);
 case "AMEX":
-P = AMEXH / (AMEXH + AMEXL) * 100;
+    diff = close(Symbol = "$AMHGH", period = ap) - close(Symbol = "$AMLOW", period = ap);
 case "ARCA":
-P = ARCAH / (ARCAH + ARCAL) * 100;
+    diff = close(Symbol = "$ARHGH", period = ap) - close(Symbol = "$ARLOW", period = ap);
 case "ETF":
-P = ETFH / (ETFH + ETFL) * 100;
+    diff = close(Symbol = "$ETFHIGH", period = ap) - close(Symbol = "$ETFLOW", period = ap);
 }
-def price = if isNaN(P) then price[1] else P;
-plot data = if isNaN(close) then double.nan else price;
-data.EnableApproximation();
-data.SetDefaultColor(Color.Cyan);
-plot avg = MovingAverage(AvgType, data, length);
-avg.EnableApproximation();
-avg.AssignValueColor(if between(avg, OverSold, OverBought)
-                     then Color.yellow
-                     else if avg >= OverBought
-                          then Color.Green
-                          else Color.Red);
-avg.SetLineWeight(2);
-plot OB = if isNaN(close) then double.nan else OverBought;
-OB.SetDefaultColor(Color.Red);
-plot OS = if isNaN(close) then double.nan else OverSold;
-OS.SetDefaultColor(Color.Green);
-plot neutral = if isNaN(close) then double.nan else 50;
-neutral.SetdefaultColor(Color.Dark_Gray);
-addCloud(0, OS, CreateColor(250,0,0), CreateColor(250,0,0));
-addCloud(OS, neutral, createColor(50,0,0), createColor(50,0,0));
-addCloud(neutral, OB, createColor(0,50,0), createColor(0,50,0));
-addCloud(OB, 100, CreateColor(0,250,0), createColor(0,250,0));
-# End High - Low Index
+ 
+plot hlp = if IsNaN(close) then Double.NaN else diff;
+hlp.EnableApproximation();
+hlp.SetPaintingStrategy(PaintingStrategy.HISTOGRAM);
+hlp.SetLineWeight(5);
+hlp.AssignValueColor(if hlp < 0 then Color.RED
+                        else if hlp > 0 then Color.GREEN
+                        else Color.CYAN);
+ 
+plot zero = 0.0;
+zero.SetDefaultColor(Color.WHITE);
+zero.SetLineWeight(1);
+zero.HideBubble();
+zero.HideTitle();
